6c157344de0bfb817453ec71a70966943969e39f
Adam Cooper <frankidyfrank3@gmail.com>
2019-09-16 11:33:43 -0500

    finnished customer view

--- bamazom-seed.sql
+++ bamazom-seed.sql
@@ -14,4 +14,31 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("42 inch TV", "electronics", 500, 20);
+VALUES ("42 inch TV", "electronics", 500, 23);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("50 inch TV", "electronics", 800, 16);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("60 inch TV", "electronics", 850, 2);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("70 inch 4K TV", "electronics", 1500, 31);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("kitchen dishes", "housewheres", 50, 12);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("baking sheet", "housewheres", 38, 11);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Dell Laptop", "electronics", 595, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("washing machine", "electronics", 495, 6);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("slimjim", "grocery", 2, 43);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("12 pk mtn dew", "grocery", 4, 20);
---
+++ bamazonCustomer.js
@@ -0,0 +1,87 @@
+const mysql = require("mysql");
+const asciiTable = require("ascii-table");
+const inquirer = require("inquirer");
+const SqlString = require('sqlstring');
+
+let con = mysql.createConnection({
+    host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "root",
+    database: "bamazon"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + con.threadId);
+    afterCon();
+});
+
+function afterCon(){
+    getProducts(`SELECT * FROM products`, function(res){
+        let table = createTable(res);
+        console.clear();
+        console.log(table.toString()+"\n")
+        askQuestions(function(answers){
+           let item = res.filter(thing => parseInt(thing.id) === parseInt(answers.id));
+            if (!item[0]) {
+                con.end();
+                return console.log("incorrect item id given: no product found!")
+            }
+            if (parseInt(answers.quantity) > parseInt(item[0].stock_quantity) || parseInt(answers.quantity) <= 0){
+                con.end();
+                return console.log("Insufficient quantity!\nOrder Canceled")
+            }
+            let totalPrice = item[0].price * parseInt(answers.quantity);
+            getProducts(`UPDATE products SET stock_quantity=${ parseInt(item[0].stock_quantity) - parseInt(answers.quantity)} WHERE id=${item[0].id}`, function(resolve){
+                getProducts(`SELECT * FROM products`, function (res) {
+                    let table = createTable(res);
+                    console.clear();
+                    console.log(table.toString()+"\n"+`Your Total is: $${totalPrice}`)
+                    con.end();
+                });
+            })
+        })
+    })
+}
+
+function askQuestions(cb){
+    inquirer.prompt([
+        {
+            name: "id",
+            message: "What is the ID of the product you would like to buy?\n",
+            validate: function(name){
+                return name !== "";
+            }
+        },
+        {
+            name: "quantity",
+            message: "how many would like to buy?\n",
+            validate: function(name){
+                return name !== "";
+            }
+        }
+    ]).then(function(answers) {
+     cb(answers)
+    })
+}
+
+function getProducts(queryString, cb) {
+    con.query(queryString, function (err, res) {
+        if (err) throw err;
+        cb(res)
+    })
+}
+
+function createTable(res){
+    let table = new asciiTable("Products")
+    table.setHeading("id", "product", "department", "price", "quantity")
+    res.forEach(item => {
+        let {id, product_name, department_name, price, stock_quantity} = item
+        table.addRow(id, product_name, department_name, price, stock_quantity)
+    });
+    return table;
+}
